27374795acd39286956e924fddf99cf796039365
pavi1998 <53079982+pavi1998@users.noreply.github.com>
2019-07-20 15:04:48 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,7 +1,7 @@
 n=input()
 if n=='a'or n=='A' or n=='e' or n=='E' or n=='i' or n=='I'    or n=='O' or n=='o' or n=='u' or n=='U':
-   print("vowel")
+   print("Vowel")
 elif n>='a'and n<='z' or n>='A' and n<='Z':
-   print("consonant")   
+   print("Consonant")   
 else:
-  print("invalid")   
+  print("Invalid")   
