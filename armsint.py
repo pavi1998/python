feef45642d0b6e2eb1956ff0aeb697e5fdf4c5c5
pavi1998 <53079982+pavi1998@users.noreply.github.com>
2019-07-28 21:11:42 +0530

    Update armsint.py

--- armsint.py
+++ armsint.py
@@ -1,12 +1,15 @@
-m,n, = list(map(int,input().strip().split()))[:2] 
-for i in range(m,n+1):
-      order = len(str(i))
-      sum = 0
-      temp = i      
-while temp > 0:
-       digit=temp % 10
-       sum +=digit ** order
-       temp //= 10
-if i== sum:
+a,b = list(map(int,input().strip().split()))[:2] 
+ 
+for i in range(a,b+ 1):
+   
+   sum = 0
+ 
+  
+   t = i
+   while t> 0:
+       d = t % 10
+       sum += d ** 3
+       t//= 10
+ 
+   if i == sum:
        print(i)
-
