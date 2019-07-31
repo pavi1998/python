66b356c22b44404cbffc649daf1b35101df67be3
pavi1998 <53079982+pavi1998@users.noreply.github.com>
2019-07-31 21:20:51 +0530

    Update week.py

--- week.py
+++ week.py
@@ -1,10 +1,6 @@
 a=str(input())
-b=str(input())
 if a=="Saturday" or a=="Sunday": 
       print('yes')
 else:
       print('no')  
-if b=="Saturday" or b=="Sunday":
-       print("yes")
-else:
-       print("no")        
+     
