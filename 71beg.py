7bd62bf6d366e87dcdebd5002a435d50f79b2f99
pavi1998 <53079982+pavi1998@users.noreply.github.com>
2019-08-15 16:36:09 +0530

    Create 71beg.py

---
+++ 71beg.py
@@ -0,0 +1,16 @@
+def reverse(s): 
+    return s[::-1] 
+  
+def isPalindrome(s): 
+    rev = reverse(s)  
+    if (s == rev): 
+        return True
+    return False
+
+s =str(input())
+a = isPalindrome(s) 
+  
+if a == 1: 
+    print("Yes") 
+else: 
+    print("No") 
