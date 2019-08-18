cad0e60b574836b5d85744ab3b20070deb029ca2
pavi1998 <53079982+pavi1998@users.noreply.github.com>
2019-08-18 20:17:03 +0530

    Create 108pl.py

---
+++ 108pl.py
@@ -0,0 +1,12 @@
+from itertools import accumulate 
+  
+def cumulativeSum(input): 
+      c=list(accumulate(input))
+      for i in c:
+        print(i,end=" ") 
+  
+# Driver program 
+if __name__ == "__main__": 
+    o=int(input())
+    input=list(map(int,input().strip().split()))[:o] 
+    cumulativeSum(input) 
